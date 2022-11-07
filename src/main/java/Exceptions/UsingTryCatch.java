c44bbd9baf6262cf1f7fb72f5e43f8c2ee7ddfff
Cillian <G00404411@tu.ie>
2022-11-07 10:15:39 +0000

    Added while loop to catch code thing

--- src/main/java/Exceptions/UsingTryCatch.java
+++ src/main/java/Exceptions/UsingTryCatch.java
@@ -5,22 +5,28 @@ import java.util.Scanner;
 
 public class UsingTryCatch {
     public static void main(String[] args) {
-        System.out.println("Please enter a number between 0 - 9 :");
-        Scanner myscan = new Scanner(System.in);
 
+        int exit = 1;
 
-        try {
-            int myNum = myscan.nextInt();
+        while(exit == 1){
+            System.out.println("Please enter a number between 0 - 9 :");
+            Scanner myscan = new Scanner(System.in);
 
-             if (myNum <= 9 && myNum >= 0) {
-                   System.out.println("You entered " + myNum);
-             }
-             else {
-                  System.out.println("Not a valid number");
-             }
-        }
-        catch (InputMismatchException e){
-            System.out.println("You have entered invalid input");
+            try {
+                int myNum = myscan.nextInt();
+
+                if (myNum <= 9 && myNum >= 0) {
+                    System.out.println("You entered " + myNum);
+                    exit = 0;
+                }
+                else {
+                    System.out.println("Not a valid number");
+                }
+
+            }
+            catch (InputMismatchException e) {
+                System.out.println("You have entered invalid input");
+            }
         }
     }
 }
